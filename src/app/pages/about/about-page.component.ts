1946e1d806d19b9188b2ee7d5c115c28d2184bc0
Roberto Mora <roberto.mora.cl@gmail.com>
2025-02-04 00:16:41 -0300

    SPA Tradicional

---
+++ src/app/pages/about/about-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AboutPageComponent {
+
+}
