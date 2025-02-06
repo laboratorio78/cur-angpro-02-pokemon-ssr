3eb88a9adedd8b8474bea62e3aeb68ee05288e95
Roberto Mora <roberto.mora.cl@gmail.com>
2025-02-06 20:29:10 -0300

    PLATFORM_ID - Cliente y Server

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
@@ -11,12 +12,20 @@ export default class PricingPageComponent implements OnInit {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    // console.log('Hola mundo');
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({ name: 'description', content: 'Este es mi Pricing Page' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
-    this.meta.updateTag({ name: 'keywords', content: 'Hola,Mundo,Fernando,Herrera,Curso,Angular,PRO' });
+
   }
 
 }
